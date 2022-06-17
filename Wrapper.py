68abe76acd6ab5f8bbfd0ef8f17fb795c7edd9c4
Rodrigo <chavezrodz@gmail.com>
2022-06-17 14:53:47 -0400

    logging scaled unscaled, generalize MLP

--- Wrapper.py
+++ Wrapper.py
@@ -97,20 +97,30 @@ class Wrapper(LightningModule):
     def training_step(self, batch, batch_idx):
         pred, y = self.predict_step(batch, batch_idx)
         batch_size = y.shape[0]
-        metrics = self.get_metrics(pred.flatten(-3, -1), y.flatten(-3, -1))
+
+        metrics_scaled = self.get_metrics(pred.flatten(-3, -1), y.flatten(-3, -1))
         self.log_dict(
-            {f'{k}/train': v for k, v in metrics.items()},
+            {f'{k}/validation/scaled': v for k, v in metrics_scaled.items()},
             on_epoch=True, on_step=False, batch_size=batch_size
             )
-        return metrics[self.criterion]
+
+        return metrics_scaled[self.criterion]
 
     def validation_step(self, batch, batch_idx):
         pred, y = self.predict_step(batch, batch_idx)
-        pred, y = self.unscale(pred), self.unscale(y)
         batch_size = y.shape[0]
-        metrics = self.get_metrics(pred.flatten(-3, -1), y.flatten(-3, -1))
+
+        metrics_scaled = self.get_metrics(pred.flatten(-3, -1), y.flatten(-3, -1))
         self.log_dict(
-            {f'{k}/validation': v for k, v in metrics.items()},
+            {f'{k}/validation/scaled': v for k, v in metrics_scaled.items()},
             on_epoch=True, on_step=False, batch_size=batch_size
             )
-        return metrics[self.criterion]
+
+        pred, y = self.unscale(pred), self.unscale(y)
+        metrics_unscaled = self.get_metrics(pred.flatten(-3, -1), y.flatten(-3, -1))
+        self.log_dict(
+            {f'{k}/validation/unscaled': v for k, v in metrics_unscaled.items()},
+            on_epoch=True, on_step=False, batch_size=batch_size
+            )
+
+        return metrics_scaled[self.criterion]
